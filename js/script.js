e84b726f639653bd8bb510ed9a8dfbff671e456f
Hieu Vu <65319926+hieuvu2719@users.noreply.github.com>
2022-05-02 17:12:21 -0400

    1st

---
+++ js/script.js
@@ -0,0 +1,126 @@
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({ 
+	filter: '*',
+	animationOptions: {
+		duration: 1500,
+		easing: 'linear',
+		queue: false
+	}
+});
+
+})
+
+
+
+
+$(document).ready(function(){
+$('#slides').superslides({
+	animation: 'fade',
+	play: 5000,
+	pagination: false
+});
+
+var typed = new Typed(".typed",{
+	strings: ["Software Developer.", "Game Developer.", "Full Stack Developer.", "Student."],
+	typeSpeed: 70,
+	loop: true,
+	startDelay: 1000,
+	showCursor: false
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+	itmes:4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        778:{
+        	items:3
+        },
+        938:{
+            items:5
+        }
+    }
+});
+
+
+
+
+var skillsTopOffset = $(".skillsSection").offset().top;
+
+$(window).scroll(function(){
+
+	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+
+			$('.chart').easyPieChart({
+			easing: 'easeInOut',
+			barColor: '#fff',
+			trackColor: false,
+			scaleColor: false,
+			lineWidth: 4,
+			size: 152	
+
+			})
+
+
+
+	}
+});
+
+
+$("[data-fancybox]").fancybox();
+
+
+
+
+$("#filters a").click(function(){
+	$("#filters .current").removeClass("current");
+	$(this).addClass("current");
+
+	var selector = $(this).attr("data-filter");
+	$(".items").isotope({
+	filter: selector,
+	animationOptions: {
+		duration: 1500,
+		easing: 'linear',
+		queue: false
+	}
+});
+
+	return false;
+})
+
+
+
+
+
+const nav = $("#navigation");
+const navTop = nav.offset().top;
+
+$(window).on("scroll", stickyNavigation);
+
+function stickyNavigation() {
+	var body = $("body");
+
+	if($(window).scrollTop() >= navTop){
+		body.css("padding-top",nav.outerHeight() + "px");
+		body.addClass("fixedNav");	
+	}
+	else{
+		body.css("padding-top",0);
+		body.removeClass("fixedNav");
+	}
+}
+
+
+});
+
